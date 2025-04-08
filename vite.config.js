e69377f571b4edb9e55677f6583b2dd82214a0f3
darlene9497 <darlene.nandabi@student.moringaschool.com>
2025-04-08 16:14:20 +0300

    :art: added sass preprocessor options

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use '/styles/_variables.scss' as *;` // adjust path based on your folder structure
+      }
+    }
+  }
 })
