ff8a123b4ee8c9a1e1a6ee9d756b85aa0e09c361
darlene9497 <darlene.nandabi@student.moringaschool.com>
2025-04-09 10:47:54 +0300

    :lipstick::iphone:styled and  made the navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,30 @@
 import './Navbar.scss'
+import { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () => setIsOpen(!isOpen)
+
   return (
-    <div>
-        <nav>
-            <ul>
-                <li>About</li>
-                <li>Services</li>
-            </ul>
-            <div className="logo">
-                Ciku Mwangi
-            </div>
-            <ul>
-                <li>Portfolio</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    </div>
+    <nav className="navbar">
+      <div className="logo">Ciku Mwangi</div>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <div className={`nav-container ${isOpen ? 'open' : ''}`}>
+        <ul className="left-links">
+          <li>About</li>
+          <li>Services</li>
+        </ul>
+
+        <ul className="right-links">
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
   )
 }
