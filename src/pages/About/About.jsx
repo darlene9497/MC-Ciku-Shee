411cd4e25f44e5c7dc5ee229b59e42f001c65c5f
darlene9497 <darlene.nandabi@student.moringaschool.com>
2025-05-12 13:03:13 +0300

    :sparkles: add about content section with heading and paragraph, enhance About page layout and styling

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,6 +6,25 @@ const About = () => {
     <>
         <AboutHero />
         <ClientsSection />
+        <div className="about-content">
+          <h2>I can’t remember a time when I wasn’t leading the room</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis 
+            lacinia mi, ac facilisis nisl egestas nec. Curabitur scelerisque, quam 
+            vitae congue semper, nulla turpis mollis augue, condimentum volutpat 
+            lorem sem quis odio. Sed a elit at sapien volutpat porta ut ut lectus. 
+            Fusce vestibulum diam quis nibh vestibulum rutrum. Mauris a enim 
+            non magna vestibulum facilisis quis nec nulla. Maecenas suscipit 
+            eleifend massa. Nunc porttitor massa felis, quis iaculis ipsum cursus at.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis 
+            lacinia mi, ac facilisis nisl egestas nec. Curabitur scelerisque, quam 
+            vitae congue semper, nulla turpis mollis augue, condimentum volutpat 
+            lorem sem quis odio. Sed a elit at sapien volutpat porta ut ut lectus. 
+            Fusce vestibulum diam quis nibh vestibulum rutrum. Mauris a enim 
+            non magna vestibulum facilisis quis nec nulla. Maecenas suscipit 
+            eleifend massa. Nunc porttitor massa felis, quis iaculis ipsum cursus at.
+          </p>
+        </div>
     </>
   );
 };
